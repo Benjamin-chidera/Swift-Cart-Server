177f108f9f83ef15ec35799f03b3407a9e6b3e60
Benjamin-chidera <benjaminchidera72@gmail.com>
2024-05-12 14:58:20 +0100

    refactor: corrected the paystack payment method

--- controllers/paystackController.js
+++ controllers/paystackController.js
@@ -25,7 +25,6 @@ export const paystack = asyncHandler(async (req, res) => {
   const reqPayStack = https
     .request(options, (resPayStack) => {
       let data = "";
-      console.log(data);
 
       resPayStack.on("data", (chunk) => {
         data += chunk;
@@ -33,13 +32,15 @@ export const paystack = asyncHandler(async (req, res) => {
 
       resPayStack.on("end", () => {
         const responseData = JSON.parse(data);
-        if (responseData.status === true) {
+        if (responseData.status === "success" || "successful") {
           // Payment initiated successfully
           res.json(responseData);
           console.log(responseData);
+          res.redirect("http://localhost:5173/order");
         } else {
           // Payment initiation failed
           res.status(400).json({ error: "Payment initiation failed" });
+          res.redirect("http://localhost:5173/checkout");
         }
       });
     })
@@ -53,12 +54,10 @@ export const paystack = asyncHandler(async (req, res) => {
 });
 
 export const getPayment = asyncHandler(async (req, res) => {
-  // const { id } = req.params; // Assuming you're passing the transaction ID as a URL parameter
-
   const options = {
     hostname: "api.paystack.co",
     port: 443,
-    path: `/transaction`, // Include the transaction ID in the path
+    path: `/transaction`,
     method: "GET",
     headers: {
       Authorization: "Bearer " + process.env.PAYSTACK_PAYMENT,
@@ -74,9 +73,9 @@ export const getPayment = asyncHandler(async (req, res) => {
 
     response.on("end", () => {
       const paymentDetails = JSON.parse(data);
-      res.json(paymentDetails); // Send the payment details back to the client
-      console.log(paymentDetails); // Log the payment details
-      res.redirect("http://localhost:5173/");
+      res.json(paymentDetails);
+      // console.log(paymentDetails);
+      // res.redirect("http://localhost:5173/");
     });
   });
 
@@ -91,9 +90,9 @@ export const getPayment = asyncHandler(async (req, res) => {
 export const verifyPaymentDetails = asyncHandler(async (req, res) => {
   const { verifyId } = req.params();
 
-   if (!verifyId) {
-     return res.status(400).json({ message: "Missing verify ID" }); // Handle missing verifyId
-   }
+  if (!verifyId) {
+    return res.status(400).json({ message: "Missing verify ID" }); // Handle missing verifyId
+  }
 
   const options = {
     hostname: "api.paystack.co",
